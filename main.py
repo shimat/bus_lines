2faad1c754cf82145da1eda44e6afaf75d32550d
shimat <schimatk@gmail.com>
2023-03-19 07:45:37 +0900

    path

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
+import numpy as np
 import pandas as pd
 import pydeck
 import streamlit as st
-import json
+from routes import load_routes_definition, convert_routes_to_pathlayer
 
 st.title("Foo")
 
-bus_stops = pd.read_csv(
-    "data/hokkaido_chuo/stops.txt",
-    usecols=("stop_name", "stop_lat", "stop_lon"))
 
-icon_data = {
+ICON_DATA = {
     "url": "https://img.icons8.com/plasticine/100/000000/marker.png",
     "width": 128,
     "height": 128,
     "anchorY": 128,
 }
 
-#icon_series = pd.Series(icon_data, index=range(len(bus_stops)))
-#st.write(icon_series)
+bus_stops = pd.read_csv(
+    "data/hokkaido_chuo/stops.txt",
+    usecols=("stop_id", "stop_name", "stop_lat", "stop_lon"))
+# st.write(bus_stops)
+bus_stops = bus_stops.groupby("stop_name").agg({
+    #"stop_id": ",".join,
+    "stop_lat": np.mean,
+    "stop_lon": np.mean,
+}).reset_index()
 
-#bus_stops["icon_data"] = json.dumps(icon_data)
-bus_stops['icon_data'] = None
+#route_names = pd.read_csv(
+#    "data/hokkaido_chuo/routes.txt",
+#    usecols=("route_id", "route_long_name", ))
+
+routes = load_routes_definition()
+pathlayer_data = convert_routes_to_pathlayer(routes, bus_stops)
+
+st.write(routes)
+st.write(pathlayer_data)
+
+bus_stops["icon_data"] = None
+icon_data_col = bus_stops.columns.get_loc("icon_data")
 for i in bus_stops.index:
-    # bus_stops['icon_data'][i] = icon_data
-    bus_stops.iat[i, -1] = icon_data
-
-#bus_stops.loc[:, 'icon_data'] = None
-#bus_stops.loc[:, 'icon_data'] = icon_data
-#bus_stops.iloc[:, -1] = icon_data
+    bus_stops.iat[i, icon_data_col] = ICON_DATA
 
 st.write(bus_stops)
-# st.write(bus_stops.dtypes)
 
 icon_layer = pydeck.Layer(
     type="IconLayer",
@@ -41,9 +50,19 @@ icon_layer = pydeck.Layer(
     size_scale=15,
     pickable=True,
 )
+path_layer = pydeck.Layer(
+    type="PathLayer",
+    data=pathlayer_data,
+    pickable=True,
+    get_color="color",
+    width_scale=20,
+    width_min_pixels=2,
+    get_path="path",
+    get_width=5,
+)
 
 deck = pydeck.Deck(
-    layers=[icon_layer],
+    layers=(icon_layer, path_layer),
     map_style="road",
     initial_view_state=pydeck.ViewState(
         latitude=43.08984839132553,
@@ -51,8 +70,7 @@ deck = pydeck.Deck(
         zoom=11,
         pitch=0,
     ),
-    tooltip={"text": "{stop_name}"})
-
+    tooltip={"text": "{stop_name}{name}"})
 st.pydeck_chart(deck)
 
 #deck.to_html("out.html")
---
+++ routes.py
@@ -0,0 +1,24 @@
+import json
+import pandas as pd
+from typing import Any, TypeAlias
+
+RoutesDefinition: TypeAlias = list[dict[str, Any]] 
+
+
+def load_routes_definition() -> RoutesDefinition:
+    with open("data/routes.json", encoding="utf-8-sig") as f:
+        return json.load(f)["routes"]
+
+
+def convert_routes_to_pathlayer(routes: RoutesDefinition, bus_stops: pd.DataFrame) -> list[dict[str, Any]]:
+    ret = [] 
+    for route in routes:
+        obj = {}
+        obj["name"] = route["name"]
+        obj["color"] = "red"
+        obj["width"] = 1
+        obj["path"] = [
+            tuple(bus_stops.query("stop_name == @stop_name")[["stop_lon", "stop_lat"]].values[0]) for stop_name in route["stops"]
+        ]
+        ret.append(obj)
+    return ret
